d31ef84dcfd2563a7b7ca1f1fb20538fa050c537
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-22 10:30:10 +0530

    restart from where i started

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,9 +4,7 @@ const AppLayout = () => {
 	return (
 		<div>
 			<div className="grid-background"></div>
-			<main className="min-h-screen container mx-auto px-8 max-w-screen-2xl">
-				<Outlet />
-			</main>
+			<Outlet />
 		</div>
 	);
 };
