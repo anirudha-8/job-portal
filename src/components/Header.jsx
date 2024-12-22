5319819b013d4145e00b22244a09c1866c70eb6c
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-22 21:10:52 +0530

    create Navbar in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<>
+			<nav className="py-5 flex justify-between items-center">
+				<Link>
+					<img
+						src="../../public/logo3.png"
+						alt="logo image"
+						className="h-14"
+					/>
+				</Link>
+				<Button variant="outline">Login</Button>
+			</nav>
+		</>
+	);
 };
 export default Header;
