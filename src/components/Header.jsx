9397ab19984a8f8da79c94d164b55a29ab282af0
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-22 22:56:24 +0530

    setup clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
-import { Button } from "./ui/button";
+import {
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	UserButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
 	return (
@@ -12,7 +17,13 @@ const Header = () => {
 						className="h-14"
 					/>
 				</Link>
-				<Button variant="outline">Login</Button>
+				{/* <Button variant="outline">Login</Button> */}
+				<SignedOut>
+					<SignInButton />
+				</SignedOut>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 			</nav>
 		</>
 	);
