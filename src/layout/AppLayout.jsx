5cdec86c83904afb531f1e5aeca0002776045548
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-22 20:50:42 +0530

    setup AppLayout for Header

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,10 +1,17 @@
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
 	return (
 		<div>
 			<div className="grid-background"></div>
-			<Outlet />
+			<main className="min-h-screen container">
+				<Header />
+				<Outlet />
+			</main>
+			<div className="text-center bg-gray-800 text-[1.2rem] py-6 mt-10">
+				&copy;JobSync Pvt. Ltd.
+			</div>
 		</div>
 	);
 };
