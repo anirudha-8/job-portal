1815078d6011fa158c081b062efd5d8a1a0a0aaf
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-24 18:45:25 +0530

    add hero section with main slogan

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
 	return (
-		<main>
-			<section>
-				<h1>Find Your Dream Job</h1>
+		<main className="flex flex-col gap-10 py-10 sm:gap-20 sm:py-20">
+			<section className="text-center">
+				<h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
+					Find Your Dream Job <span>and get Hired</span>
+				</h1>
+				<p className="text-gray-300 text-xs sm:mt-4 sm:text-xl">
+					Explore thousands of job listings or find the perfect
+					candidate
+				</p>
 			</section>
-			<div>{/* buttons */}</div>
+
 			{/* carousel */}
 			{/* banner */}
 			<section>{/* cares */}</section>
