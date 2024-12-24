6b049042d9389971878d81c05efa107ceace0fd4
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-24 22:06:28 +0530

    add user authentication sign in modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
-import {
-	SignedIn,
-	SignedOut,
-	SignInButton,
-	UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import { useState } from "react";
 
 const Header = () => {
+	const [showSignInModal, setShowSignInModal] = useState(false);
+	// to remove the "showSignInModal", if clicked outside of the modal
+	const handleOverlayClick = (e) => {
+		if (e.target === e.currentTarget) {
+			setShowSignInModal(false);
+		}
+	};
 	return (
 		<>
 			<nav className="py-5 flex justify-between items-center">
@@ -18,14 +22,43 @@ const Header = () => {
 						className="h-14"
 					/>
 				</Link>
-				<Button variant="outline">Login</Button>
-				{/* <SignedOut>
-					<SignInButton />
-				</SignedOut>
-				<SignedIn>
-					<UserButton />
-				</SignedIn> */}
+				<div className="flex gap-8">
+					<SignedOut>
+						{/* when click on "Login", show "sign-in" modal */}
+						<Button
+							variant="outline"
+							onClick={() => setShowSignInModal(true)}
+						>
+							Login
+						</Button>
+					</SignedOut>
+					<SignedIn>
+						<Link to={"/post-job"}>
+							<Button
+								variant="destructive"
+								className="rounded-full"
+							>
+								<PenBox size={20} />
+								Post a Job
+							</Button>
+						</Link>
+						<UserButton />
+					</SignedIn>
+				</div>
 			</nav>
+
+			{/* code to show "sign-in" modal */}
+			{showSignInModal && (
+				<div
+					className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50"
+					onClick={handleOverlayClick}
+				>
+					<SignIn
+						signUpForceRedirectUrl="/onboarding"
+						signUpFallbackRedirectUrl="/onboarding"
+					/>
+				</div>
+			)}
 		</>
 	);
 };
