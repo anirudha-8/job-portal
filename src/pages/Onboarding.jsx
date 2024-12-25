842c90730b2ee26ce5663d2c56945ec9b7c7376b
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-25 12:56:55 +0530

    setup loader for onboarding page

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,4 +1,12 @@
+import { useUser } from "@clerk/clerk-react";
+
 const Onboarding = () => {
+	const { user, isLoaded } = useUser();
+
+	if (!isLoaded) {
+		return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+	}
+
 	return;
 };
 export default Onboarding;
