8329efbc0d61ca3813e8065d022e9d61029eacdb
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-26 14:17:10 +0530

    add logic to protect other routes, if user has no 'role' selected

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,10 +3,23 @@ import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
 	const { user, isLoaded, isSignedIn } = useUser();
+
 	const { pathName } = useLocation();
+
 	if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
 		return <Navigate to={"/?sign-in=true"} />;
 	}
+
+	// prevent from accessing other routes,
+	// if user has NO role selected
+	if (
+		user !== undefined &&
+		!user?.unsafeMetadata?.role &&
+		pathName !== "/onboarding"
+	) {
+		<Navigate to={"/onboarding"} />;
+	}
+
 	return children;
 };
 export default ProtectedRoute;
