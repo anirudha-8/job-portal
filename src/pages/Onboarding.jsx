6472c6c0fbe2184d4453ed711259c3b1ae141f3c
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-26 14:13:50 +0530

    add logic for role selection

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,11 +1,35 @@
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { BarLoader } from "react-spinners";
 
 const Onboarding = () => {
 	const { user, isLoaded } = useUser();
 	console.log(user);
 
+	// logic for role selection and secure router upon role selected
+	const navigate = useNavigate();
+	const handleRoleSelection = async (role) => {
+		await user
+			.update({
+				unsafeMetadata: { role },
+			})
+			.then(() => {
+				navigate(role === "recruiter" ? "/post-job" : "/jobs");
+			});
+	};
+	useEffect(() => {
+		if (user?.unsafeMetadata?.role) {
+			navigate(
+				user?.unsafeMetadata?.role === "recruiter"
+					? "/post-job"
+					: "/jobs"
+			);
+		}
+	}, [user]);
+
+	// to show loader, if any case of delay
 	if (!isLoaded) {
 		return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
 	}
@@ -16,10 +40,18 @@ const Onboarding = () => {
 				I am a...
 			</h1>
 			<div className="w-full grid grid-cols-2 gap-4 md:px-40 mt-10">
-				<Button variant="blue" className="h-36 text-2xl">
+				<Button
+					variant="blue"
+					className="h-36 text-2xl"
+					onClick={() => handleRoleSelection("candidate")}
+				>
 					Candidate
 				</Button>
-				<Button variant="destructive" className="h-36 text-2xl">
+				<Button
+					variant="destructive"
+					className="h-36 text-2xl"
+					onClick={() => handleRoleSelection("recruiter")}
+				>
 					Recruiter
 				</Button>
 			</div>
